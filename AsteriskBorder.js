c925970ee55adc80d8ddb3f1a7bc7e4fe425a6e8
Daniel Acosta <danielacosta10@aol.com>
2021-10-16 19:05:50 -0700

    more practice

---
+++ AsteriskBorder.js
@@ -0,0 +1,12 @@
+let i = 0;
+astBor = "**";
+const asteriskBorder = (...param1) => {
+    while (i < param1.length) {
+        i++;
+        astBor += "*";
+    };
+    return astBor;
+    
+    
+};
+
